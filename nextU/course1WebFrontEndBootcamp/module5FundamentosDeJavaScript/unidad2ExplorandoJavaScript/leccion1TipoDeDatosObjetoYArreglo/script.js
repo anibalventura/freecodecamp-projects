bd8a30a2f53f7464f02b350011bb30b82270055a
Jose Ventura <j.ventura@gruporamos.com>
2019-10-30 15:49:15 -0400

    finish exercise from lesson 1 - unit 2 of module 5

---
+++ nextU/course1WebFrontEndBootcamp/module5FundamentosDeJavaScript/unidad2ExplorandoJavaScript/leccion1TipoDeDatosObjetoYArreglo/script.js
@@ -0,0 +1,38 @@
+function stringValido(string, largo) {
+    if (string == null || string == undefined) {
+        return false;
+    }
+    if (largo && string.trim().length < largo) {
+        return false;
+    }
+    return true;
+}
+
+function fechaValida(fecha, minimaFecha) {
+    if (fecha == null || fecha == undefined) {
+        return false;
+    }
+    if (minimaFecha && fecha.getTime() < minimaFecha.getTime()) {
+        return false;
+    }
+    return true;
+}
+
+function eventoValido(evento) {
+    if (evento == null || evento == undefined) {
+        return false;
+    }
+    return (
+        stringValido(evento.nombre, 3) &&
+        stringValido(evento.descripcion, 10) &&
+        fechaValida(evento.fecha, new Date(2018, 0, 1, 0, 0, 0))
+    );
+}
+
+var evento = {
+    nombre: "Evento 1",
+    descripcion: "Esto es una descripción",
+    fecha: new Date(2018, 10, 1)
+};
+
+console.log(eventoValido(evento));
