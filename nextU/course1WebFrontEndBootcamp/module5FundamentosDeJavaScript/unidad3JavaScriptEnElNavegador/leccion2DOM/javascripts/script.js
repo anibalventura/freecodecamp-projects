d878af3ff4fcfb6d5af5a45252d264b70777b37e
Jose Ventura <j.ventura@gruporamos.com>
2019-11-06 15:24:26 -0400

    Adding exercise from lesson 2 unit 3 of module 5

---
+++ nextU/course1WebFrontEndBootcamp/module5FundamentosDeJavaScript/unidad3JavaScriptEnElNavegador/leccion2DOM/javascripts/script.js
@@ -0,0 +1,12 @@
+function validar(formulario) {
+
+  if (formulario.nombre.value.trim().length == 0) {
+    document.getElementById("errorNombre").innerText = "Campo obligatorio";
+    formulario.nombre.focus();
+    return false;
+  }
+
+  alert("Datos enviados");
+
+  return true;
+}
