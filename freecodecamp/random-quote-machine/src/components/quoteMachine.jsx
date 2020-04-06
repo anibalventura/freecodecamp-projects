9f6ec9d42124f39eef4bcb69e2c1a2ac4b69ddcb
anibalventura <anibalventura20@gmail.com>
2020-04-05 23:06:40 -0400

    Create ShareButtons component

--- freecodecamp/random-quote-machine/src/components/quoteMachine.jsx
+++ freecodecamp/random-quote-machine/src/components/quoteMachine.jsx
@@ -1,12 +1,10 @@
 import React from "react";
+import ShareButtons from "./shareButtons";
 import { Card } from "@material-ui/core";
 import { CardActions } from "@material-ui/core";
 import { CardContent } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import { Button } from "@material-ui/core";
-import { IconButton } from "@material-ui/core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const QuoteMachine = ({ selectedQuote, handleNextQuote }) => {
   return (
@@ -27,15 +25,7 @@ const QuoteMachine = ({ selectedQuote, handleNextQuote }) => {
           >
             Next Quote
           </Button>
-          <IconButton
-            id="tweet-quote"
-            target="_blank"
-            href={encodeURI(
-              `https://twitter.com/intent/tweet?text="${selectedQuote.quote}" - ${selectedQuote.author}&hashtags=quoteoftheday`
-            )}
-          >
-            <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
-          </IconButton>
+          <ShareButtons selectedQuote={selectedQuote} />
         </CardActions>
       </Card>
     </React.Fragment>
