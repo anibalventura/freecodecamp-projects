bde48bbd54f58c7bc7e8e59fa8d79fbf072dac50
anibalventura <anibalventura20@gmail.com>
2020-04-06 22:18:00 -0400

    Add timeLeft component and beep audio

--- freecodecamp/pomodoro-clock/src/App.js
+++ freecodecamp/pomodoro-clock/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Break from "./components/break";
 import Session from "./components/session";
 import TimeLeft from "./components/timeLeft";
 
 const App = () => {
+  const audioElement = useRef(null);
+  const [currentSessionType, setCurrentSessionType] = useState("Session"); //Session or break
+  const [intervalId, setIntervalId] = useState(null);
   const [breakLength, setBreakLength] = useState(300);
   const [sessionLength, setSessionLength] = useState(60 * 25);
+  const [timeLeft, setTimeLeft] = useState(sessionLength);
+
+  //Use useEffect react hook function to change the time left
+  useEffect(() => {
+    setTimeLeft(sessionLength);
+  }, [sessionLength]);
 
   //Decrease the value of setBreakLength by 1 minute = 60 secs
   const decrementBreakLenght = () => {
@@ -37,6 +46,62 @@ const App = () => {
     setSessionLength(sessionLength + 60);
   };
 
+  const isStarted = intervalId != null;
+  const hanldeStartStop = () => {
+    if (isStarted) {
+      //If we are in started mode:
+      //stop the timer
+      clearInterval(intervalId);
+      setIntervalId(null);
+    } else {
+      //If we are in stopped mode:
+      //decrement timeLeft every one second (1000 ms)
+      const newIntervalId = setInterval(() => {
+        setTimeLeft((prevTimeLeft) => {
+          const newTimeLeft = prevTimeLeft - 1;
+          if (newTimeLeft >= 0) {
+            return prevTimeLeft - 1;
+          }
+          //Time left is less than zero
+          audioElement.current.play();
+          //If session:
+          if (currentSessionType === "Session") {
+            //Switch to break
+            setCurrentSessionType("Break");
+            //setTimeLeft to breakLength
+            setTimeLeft(breakLength);
+          }
+
+          //If break:
+          else if (currentSessionType === "Break") {
+            //Switch to session
+            setCurrentSessionType("Session");
+            //setTimeLeft to sessionLength
+            setTimeLeft(sessionLength);
+          }
+        });
+      }, 10);
+      TODO: setIntervalId(newIntervalId);
+    }
+  };
+
+  const hanldeReset = () => {
+    //Reset the audio
+    audioElement.current.load();
+    //Clear the timeout interval
+    clearInterval(intervalId);
+    //Set intervalId null
+    setIntervalId(null);
+    //set the sessionType to 'Session'
+    setCurrentSessionType("Session");
+    //Reset the session leght to 25 minutes
+    setSessionLength(60 * 25);
+    //Reset the sessionLength to 5 minutes
+    setBreakLength(60 * 5);
+    //Reset te timer to 25 minutes
+    setTimeLeft(60 * 25);
+  };
+
   return (
     <React.Fragment>
       <Break
@@ -44,12 +109,28 @@ const App = () => {
         decrementBreakLenght={decrementBreakLenght}
         incrementBreakLenght={incrementBreakLenght}
       />
-      <TimeLeft breakLength={breakLength} sessionLength={sessionLength} />
+      <TimeLeft
+        breakLength={breakLength}
+        sessionLength={sessionLength}
+        timerLabel={currentSessionType}
+        hanldeStartStop={hanldeStartStop}
+        startStopLabel={isStarted ? "Stop" : "Start"}
+        timeLeft={timeLeft}
+      />
       <Session
         sessionLength={sessionLength}
         decrementSessionLenght={decrementSessionLenght}
         incrementSessionLenght={incrementSessionLenght}
       />
+      <button id="reset" onClick={hanldeReset}>
+        Reset
+      </button>
+      <audio
+        src="sounds/beep.mp3"
+        type="audio/mpeg"
+        id="beep"
+        ref={audioElement}
+      ></audio>
     </React.Fragment>
   );
 };
--- freecodecamp/pomodoro-clock/src/components/timeLeft.jsx
+++ freecodecamp/pomodoro-clock/src/components/timeLeft.jsx
@@ -1,70 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
-import { useEffect } from "react";
 
 momentDurationFormatSetup(moment);
 
-const TimeLeft = ({ breakLength, sessionLength }) => {
-  const [currentSessionType, setCurrentSessionType] = useState("Session"); //Session or break
-  const [intervalId, setIntervalId] = useState(null);
-  const [timeLeft, setTimeLeft] = useState(sessionLength);
-
-  //Use useEffect react hook function to change the time left
-  useEffect(() => {
-    setTimeLeft(sessionLength);
-  }, [sessionLength]);
-
-  const isStarted = intervalId != null;
-  const hanldeStartStop = () => {
-    if (isStarted) {
-      //If we are in started mode:
-      //stop the timer
-      clearInterval(intervalId);
-      setIntervalId(null);
-    } else {
-      //If we are in stopped mode:
-      //decrement timeLeft every one second (1000 ms)
-      const newIntervalId = setInterval(() => {
-        setTimeLeft((prevTimeLeft) => {
-          const newTimeLeft = prevTimeLeft - 1;
-          if (newTimeLeft >= 0) {
-            return prevTimeLeft - 1;
-          }
-
-          //If session:
-          if (currentSessionType === "Session") {
-            //Switch to break
-            setCurrentSessionType("Break");
-            //setTimeLeft to breakLength
-            setTimeLeft(breakLength);
-          }
-
-          //If break:
-          else if (currentSessionType === "Break") {
-            //Switch to session
-            setCurrentSessionType("Session");
-            //setTimeLeft to sessionLength
-            setTimeLeft(sessionLength);
-          }
-        });
-      }, 1000);
-      setIntervalId(newIntervalId);
-    }
-  };
-
+const TimeLeft = ({
+  hanldeStartStop,
+  startStopLabel,
+  timeLeft,
+  timerLabel,
+}) => {
   //Format the time with moment.js package
-  const formattedTimeLeft = moment
-    .duration(timeLeft, "s")
-    .format("mm:ss", { trim: false });
+  const formattedTimeLeft = moment.duration(timeLeft, "s").format("mm:ss", {
+    trim: false,
+  });
   return (
     <React.Fragment>
       <div>
         {formattedTimeLeft}
-        <p id="timer-label">{currentSessionType}</p>
-        <button onClick={hanldeStartStop}>
-          {isStarted ? "Stop" : "Start"}
-        </button>
+        <p id="timer-label">{timerLabel}</p>
+        <p id="time-left">{formattedTimeLeft}</p>
+        <button onClick={hanldeStartStop}>{startStopLabel}</button>
       </div>
     </React.Fragment>
   );
