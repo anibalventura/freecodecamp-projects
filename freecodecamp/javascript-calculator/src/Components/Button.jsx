ac02feed0d13c4ce32785eac123fc9298dd71a42
anibalventura <anibalventura20@gmail.com>
2020-04-09 23:53:27 -0400

    Fix calculate method and fill fcc test

--- freecodecamp/javascript-calculator/src/Components/Button.jsx
+++ freecodecamp/javascript-calculator/src/Components/Button.jsx
@@ -2,20 +2,15 @@ import React, { Component } from "react";
 
 class Button extends Component {
   sendToParent = () => {
-    this.props.handleButtonClick(this.props.name);
+    this.props.handleButton(this.props.name);
   };
 
   render() {
-    const { name } = this.props;
+    const { name, id } = this.props;
     return (
-      <React.Fragment>
-        <button
-          onClick={this.sendToParent}
-          className="btn"
-        >
-          {name}
-        </button>
-      </React.Fragment>
+      <button id={id} onClick={this.sendToParent} className="btn">
+        {name}
+      </button>
     );
   }
 }
--- freecodecamp/javascript-calculator/src/Components/Screen.jsx
+++ freecodecamp/javascript-calculator/src/Components/Screen.jsx
@@ -2,13 +2,8 @@ import React, { Component } from "react";
 
 class Screen extends Component {
   render() {
-    const { current, result } = this.props;
-    return (
-      <div id="display">
-        <p>{current}</p>
-        <p>{result}</p>
-      </div>
-    );
+    const { currentNumber, id } = this.props;
+    return <div id={id}>{currentNumber}</div>;
   }
 }
 
