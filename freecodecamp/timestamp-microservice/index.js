81decadcb57fb9f3b9c086e106ff74633e2ffb30
Anibal Ventura <anibalventura20@gmail.com>
2020-04-23 16:20:00 -0400

    Fix fCC tests

--- freecodecamp/timestamp-microservice/index.js
+++ freecodecamp/timestamp-microservice/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
 app.get("/api/timestamp/:dateString?", (request, response) => {
   const dateString = request.params.dateString;
